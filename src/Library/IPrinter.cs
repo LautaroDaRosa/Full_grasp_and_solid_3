7432ecbc96c1a76d187f716c5c371e50b3e1e224
LautaroDaRosa <LautaroDaRosa@users.noreply.github.com>
2020-10-25 20:07:24 -0300

    comentarios patrones

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -1,5 +1,12 @@
 namespace Full_GRASP_And_SOLID.Library
 {
+    //Al usar esta interfaz para ambos tipos de impresión evitamos preguntar por el destino
+    //cumpliendo así con el patrón de Polimorfismo.
+    //Esta interfaz al igual que las dos clases que la implementan cumplen con el patrón de SRP
+    //porque sólo tienen la función de impresión.
+    //También se cumple con el principio de OCP ya que si se quiere agregar otro método de impresión
+    //sólo es necesario crear una clase que implemente la interfaz IPrinter, sin necesidad
+    //de modificar ésta o cualquier otra clase.
     public interface IPrinter
     {
         void PrintRecipe(Recipe recipe);
